a6694cc1a9da4960ebf3250170bae46aa60a2f48
ixouu <julienchanon@gmail.com>
2022-10-24 15:37:05 +0200

    title and button has been reworked

--- src/components/Header/Index.jsx
+++ src/components/Header/Index.jsx
@@ -7,8 +7,8 @@ const Header = () => {
             <div className='header-filter'>
                 <h1>La vie est comme une montre dont chaque seconde est précieuse</h1>
                 <div className='header-btns'>
-                    <ButtonComponent title="Toutes les montres" color="#F1F1F1" link="categories" class1="btn header-btn" height="60px"/>
-                    <ButtonComponent title="Catégories" link="categories" color="#F1F1F1" class1="btn header-btn"/>
+                    <ButtonComponent title="Toutes les montres" color="rgb(18, 18, 18)" link="categories" class1="btn header-btn" height="60px"/>
+                    <ButtonComponent title="Catégories" link="categories" color="rgb(18, 18, 18)" class1="btn header-btn"/>
                 </div>
                 
             </div>
