3d40bcea3e73eb01f475f3fe0acba7751b72d05a
ixouu <julienchanon@gmail.com>
2022-11-06 01:12:02 +0100

    modal error and success div has been added

--- src/components/Modals/AddToCartModal/AddToCartModal.jsx
+++ src/components/Modals/AddToCartModal/AddToCartModal.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const AddToCartModal = ({open, onClose}) => {
+const AddToCartModal = ({open, onClose, successHandlter, productName}) => {
+  console.log(successHandlter)
   return ReactDOM.createPortal(
-    <div>AddToCartModal</div>,
-    document.getElementById('modal-root')
+    <>
+      <div className='overlay' onClick={onClose}></div>
+      <div className='hero-modal'>
+      {successHandlter 
+      ? (<div className='addToCart-container addToCart-success'>
+          <span><i class="fa-solid fa-circle-check"></i> La montre {productName} a été rajouté au panier.</span>
+        </div>) 
+        : (<div className='addToCart-container addToCart-failed'>
+          <span><i class="fa-solid fa-circle-xmark"></i>Votre article n'a pas été rajouté au panier, veuillez réessayer.</span>
+          </div>)
+      }
+      <div className="cartPreview">
+        <h3>Votre panier : </h3>
+      </div>
+      </div>
+    </>,document.getElementById('modal-root')
   )
 }
 
--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -2,11 +2,16 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { client , urlFor } from "../../lib/client";
 
-import ButtonComponent from "../../components/ButtonComponent/ButtonComponent";
-import Star from '../../components/Star/Star'
+import Star from '../../components/Star/Star';
+import AddToCartModal from "../../components/Modals/AddToCartModal/AddToCartModal";
 
+import { useStateContext } from '../../context/stateContext';
+import { StateContext } from "../../context/stateContext";
 
 const Product = () => {
+
+    const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuanitity, onRemove } = useStateContext();
+
 
     const [productData, setProductData] = useState({
         title: '',
@@ -98,97 +103,112 @@ const Product = () => {
     
   }
 
-  // Add to cart function
+  // ADD TO CART 
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [value, setValue] = useState(0);
+
   const addToCart = (e) => {
     e.preventDefault();
-    window.alert("ok")
+    // Check the input value
+    if (value <= 0 || value > productData.availableStock){
+        setSuccess(false);
+    }else{
+        setSuccess(true);
+    }
+    // localStorage.set("cart", JSON.stringify)
+   
+    !modalIsOpen? setModalIsOpen(true) : setModalIsOpen(false);
   }
 
 
     return (
+        <>
         <main className="main-product">
             {error && <p>{error}</p> }
             {isLoading
             ?<p>Chargement...</p>
             :(
                 <>
-                <div className="product-container">
-                    <div className="product-container_left">
-                    <img 
-                        src={urlFor(productData.image[0])}
-                        alt={`Montre ${productData.title}`}
-                        className='product-card_img'
-                        height={400}
-                        ></img>
+                    <div className="product-container">
+                        <div className="product-container_left">
+                        <img 
+                            src={urlFor(productData.image[0])}
+                            alt={`Montre ${productData.title}`}
+                            className='product-card_img'
+                            height={400}
+                            ></img>
+                        </div>
+                        <div className="product-container_right" id={productData.id}>
+                            <div className="product-informations">
+                                <h1 className="product-title">{productData.title}</h1>
+                                <h2 className="product-brand">{productData.brand}</h2>
+                                <div className="product-price_container">
+                                    <span className="product-price">{productData.price},</span>
+                                    <span className="product-price_decimal">00 </span>
+                                    <span className="product-price_sign">€</span>
+                                </div> 
+                                <span className="product-price_decimal">TTC</span>   
+                                <div className="product-paimentFacilities"></div>
+                                {detailsAreExpand 
+                                    ?(<p className="product-desc">{productData.details}</p>)
+                                    :(<><p className="product-desc">{productData.details.slice(0, 200)}</p><button className="expandDetailsBtn" onClick={(e) => {setDetailsAreExpand(!detailsAreExpand); e.preventDefault()}}>lire la suite..</button></>)
+                                }
+                            </div>
+                            <div className="product-addToCart">
+                                <form>
+                                    <div className="formQty">
+                                        <label htmlFor="product-qty">Quantité</label>
+                                        <input type="number" id="product-qty" min="1" max={productData.availableStock}defaultValue="1" onChange={(e) => setValue(Number(e.target.value))}/>
+                                    </div>
+                                    <div className="formBtn">
+                                        <button className="btn product-button" onClick={(e) => addToCart(e)}>AJOUTER AU PANIER</button>
+                                        {displayStock(productData.availableStock)}
+                                    </div>
+                                </form>
+                            </div>
+                            <div className="reassurance">
+                            <ul>
+                                <li><div className="reassurance-item"><span><i className="fa-solid fa-truck"></i> Expédié sous 24h</span></div></li>
+                                <li><div className="reassurance-item"><span><i className="fa-solid fa-thumbs-up"></i>Satisfait ou remboursé</span></div></li>
+                                <li><div className="reassurance-item"><span><i className="fa-solid fa-shield"></i>Garantie 2 ans</span></div></li>
+                            </ul>
+                        </div>
+                        </div>
+                    
                     </div>
-                    <div className="product-container_right" id={productData.id}>
-                        <div className="product-informations">
-                            <h1 className="product-title">{productData.title}</h1>
-                            <h2 className="product-brand">{productData.brand}</h2>
-                            <div className="product-price_container">
-                                <span className="product-price">{productData.price},</span>
-                                <span className="product-price_decimal">00 </span>
-                                <span className="product-price_sign">€</span>
-                            </div> 
-                            <span className="product-price_decimal">TTC</span>   
-                            <div className="product-paimentFacilities"></div>
-                            {detailsAreExpand 
-                                ?(<p className="product-desc">{productData.details}</p>)
-                                :(<><p className="product-desc">{productData.details.slice(0, 200)}</p><button className="expandDetailsBtn" onClick={(e) => {setDetailsAreExpand(!detailsAreExpand); e.preventDefault()}}>lire la suite..</button></>)
+                    <div className="tabs">
+                        <ul className="tabs-list">
+                            <li onClick={() => showTabs("desc")}>
+                                <span>Description</span>
+                            </li>
+                            <li onClick={() => showTabs("rating")}>
+                            <span>Avis client</span>
+                            
+                            </li>
+                        </ul>
+                        <div className="tabs-details">
+                            {ratingIsActive && 
+                                <div className="rating">
+                                    {renderStar(1)}
+                                    {renderStar(2)}
+                                    {renderStar(3)}
+                                    {renderStar(4)}
+                                    {renderStar(5)}
+                                    <span>({productData.popularity})</span>
+                                </div>
+                            }
+                            {descIsActive &&
+                                <section>{productData.details}</section>
                             }
                         </div>
-                        <div className="product-addToCart">
-                            <form>
-                                <div className="formQty">
-                                    <label htmlFor="product-qty">Quantité</label>
-                                    <input type="number" id="product-qty" min="1" max={productData.availableStock}defaultValue="1"/>
-                                </div>
-                                <div className="formBtn">
-                                    <button className="btn product-button" onClick={(e) => addToCart(e)}>AJOUTER AU PANIER</button>
-                                    {displayStock(productData.availableStock)}
-                                </div>
-                            </form>
-                        </div>
-                        <div className="reassurance">
-                        <ul>
-                            <li><div className="reassurance-item"><span><i className="fa-solid fa-truck"></i> Expédié sous 24h</span></div></li>
-                            <li><div className="reassurance-item"><span><i className="fa-solid fa-thumbs-up"></i>Satisfait ou remboursé</span></div></li>
-                            <li><div className="reassurance-item"><span><i className="fa-solid fa-shield"></i>Garantie 2 ans</span></div></li>
-                        </ul>
                     </div>
-                    </div>
-                  
-                </div>
-                <div className="tabs">
-                    <ul className="tabs-list">
-                        <li onClick={() => showTabs("desc")}>
-                            <span>Description</span>
-                        </li>
-                        <li onClick={() => showTabs("rating")}>
-                        <span>Avis client</span>
-                        
-                        </li>
-                    </ul>
-                    <div className="tabs-details">
-                        {ratingIsActive && 
-                            <div className="rating">
-                                {renderStar(1)}
-                                {renderStar(2)}
-                                {renderStar(3)}
-                                {renderStar(4)}
-                                {renderStar(5)}
-                                <span>({productData.popularity})</span>
-                            </div>
-                        }
-                        {descIsActive &&
-                            <section>{productData.details}</section>
-                        }
-                    </div>
-                </div>
-                </>
-            )
-             }
-        </main>
+                    </>
+                )
+                }
+            </main>
+            {modalIsOpen && <AddToCartModal open={modalIsOpen} onClose={() => setModalIsOpen(false)} successHandlter={success} productName={productData.title}/>}
+        </>
     );
 }
 
