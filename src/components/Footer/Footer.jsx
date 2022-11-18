28a1003fa1fd3fe179fc2be46bfab92276787cf1
ixouu <julienchanon@gmail.com>
2022-11-18 19:07:32 +0100

    readme update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
 	return (
 		<footer>
 			<span>&#169; Julien Chanon</span>
+			<span className="footer-disclamer">Les images de montres provienent et appartiennent du site <a href="http://1001-montres.fr/" target="_blank" rel="noreferrer">http://1001-montres.fr/</a></span>
 		</footer>
 	);
 };
 
-export default Footer;
+export default Footer; 
